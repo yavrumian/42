ebf64f545d4eab105e329781ce4d3030c2d37c3e
Vahe Yavrumian <vaheyavrumyan@gmail.com>
2022-11-11 13:36:25 +0400

    updates

--- c00/07/ft_putnbr2.c
+++ c00/07/ft_putnbr2.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdio.h>
-int abs(int i) {
+int ft_abs(int i) {
 	if(i < 0){
 		return -i;
 	}else {
 		return i;
 	}
 }
-int reverser(int num, int reverse){
+/*int reverser(int num, int reverse){
 	if(num == 0){
 		return reverse;
 	}
@@ -16,34 +16,56 @@ int reverser(int num, int reverse){
 	num /= 10;
 	reverse = reverse * 10 + last_digit;
 	return reverser(num, reverse);
+}*/
+int ft_pow(int base, int exp){
+	int res = 1;
+	while(exp > 0){
+		res *= base;
+		exp--;
+	}
 }
-int num_size(int num, int size ){
+int ft_size(int num, int size ){
 	if (num == 0){
 		return size;
 	}
 	num /= 10;
 	size++;
-	return num_size(num, size);
+	return ft_size(num, size);
 }
 void ft_putnbr(int nb){
-	int rev_nb = abs(reverser(nb, 0));
+	//int rev_nb = abs(reverser(nb, 0));
 	char c;
+	int orig_nb = nb;
 	char zero = '0';
 	char min = '-';
+	int div = ft_pow(10, ft_size(nb, 0) - 1);
+	/*if(nb == 0){
+		write (1, &zero, 1);
+	}*/
 	if(nb < 0){
 		write(1, &min, 1);
 	}
-	while(rev_nb != 0){
-		c = rev_nb % 10 + '0';
-		rev_nb /= 10;
+	while(nb != 0){
+		if(ft_size(nb % div, 0) - ft_size(nb, 0) == 1){
+			c = ft_abs(nb / div) + '0';
+			nb %= div;
+			div /= 10;
+		}else{
+			c = ft_abs(nb % div);
+			nb /= div;
+			div /= 10;
+		}
 		write(1, &c, 1);
 	}
-	if(num_size(nb, 0) != num_size(rev_nb, 0)){
+	if(orig_nb % 10 == 0){
 		write(1, &zero, 1);
 	}
+	/*if(num_size(nb, 0) != num_size(rev_nb, 0)){
+		write(1, &zero, 1);
+	}*/
 }
 
 int main(){
-	ft_putnbr(2147483647);
+	ft_putnbr(100);
 	return 0;
 }
