e86ab371c63f8503a6b10d7bcaee5161fca95a6a
vaheyavrumyan@gmail.com <Vahe Yavrumyan>
2022-11-16 04:02:31 -0800

    c02 00, 01

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+int	ft_len(char *str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+		;
+	return (i);
+}
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int	i;
+	int	nn;
+	int	src_len;
+
+	nn = n;
+	src_len = ft_len(src);
+	i = -1;
+	while (++i < nn)
+	{
+		if (i < src_len)
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+	}
+	return (dest);
+}
