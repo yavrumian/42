24e801b9ebc0d95ce74c1c9a3866988fbf011cab
Vahe Yavrumian <vaheyavrumyan@gmail.com>
2022-11-27 15:35:49 +0400

    hey

--- rush02/parse.c
+++ rush02/parse.c
@@ -31,20 +31,78 @@ int	lines(char *str)
 	return (line);
 }
 
-char	***parse(char *str)
+void	norm(int *i, int *count, char *str)
 {
-	int		i;
-	int		nol;
-	int		line;
-	int		count;
+	
+	while (str[*i] == ' ' || str[*i] == '\t'
+		|| str[*i] == '\v' || str[*i] == '\f')
+		++(*i);
+	*count = 0;
+	while (str[*i] > 32 && str[*i] < 127)
+	{
+		++(*count);
+		++(*i);
+	}
+}
+
+char	***init(int *i, int *line, int *nol, char *str)
+{
 	char	***wrap;
 
-	nol = lines(str);
-	wrap = (char ***)malloc(nol * sizeof(char *));
+	*i = 0;
+	*nol = lines(str);
+	*line = 0;
+	wrap = (char ***)malloc(*nol * sizeof(char *));
 	if (!wrap)
 		return (NULL);
-	i = 0;
-	line = 0;
+}
+
+char	*norm2(int *count, char *str, int *i, char ***wrap)
+{
+	*count = 0;
+	if (!(str[*i] >= '0' && str[*i] <= '9'))
+		return (NULL);
+	while (str[*i] >= '0' && str[*i] <= '9')
+	{
+		++(*count);
+		++(*i);
+	}
+	return ("");
+}
+
+char	*norm3(char **wrap, int count, int *i, char *str)
+{
+	wrap[0] = malloc(count + 1);
+	if (!wrap[0])
+		return (NULL);
+	ft_strlcpy(wrap[0], &str[*i - count], count + 1);
+	while (str[*i] == ' ' || str[*i] == '\t'
+		|| str[*i] == '\v' || str[*i] == '\f')
+		++(*i);
+	if (str[(*i)++] != ':')
+		return (NULL);
+	return ("");
+}
+
+char	*norm4(char **wrap, int count, int i, char *str)
+{
+	wrap[1] = malloc(count + 1);
+	if (!wrap[1])
+		return (NULL);
+	ft_strlcpy(wrap[1], &str[i - count], count + 1);
+	if (str[i] != '\n')
+		return (NULL);
+	return ("");
+}
+
+char	***parse(char *str, int i, int count, int line)
+{
+	int		nol;
+	char	***wrap;
+
+	wrap = init(&i, &line, &nol, str);
+	if (!wrap)
+		return (NULL);
 	while (str[i])
 	{
 		wrap[line] = (char **)malloc(2 * sizeof(char *));
@@ -52,39 +110,11 @@ char	***parse(char *str)
 			return (NULL);
 		while (str[i] != '\n')
 		{
-			count = 0;
-			if (!(str[i] >= '0' && str[i] <= '9'))
+			if (!norm2(&count, str, &i, wrap)
+				|| !norm3(wrap[line], count, &i, str))
 				return (NULL);
-			while (str[i] >= '0' && str[i] <= '9')
-			{
-				++count;
-				++i;
-			}
-			wrap[line][0] = malloc(count + 1);
-			if (!wrap[line][0])
-				return (NULL);
-			ft_strlcpy(wrap[line][0], &str[i - count], count + 1);
-			while (str[i] == ' ' || str[i] == '\t'
-				|| str[i] == '\v' || str[i] == '\f')
-				++i;
-			if (str[i] == ':')
-				++i;
-			else
-				return (NULL);
-			while (str[i] == ' ' || str[i] == '\t'
-				|| str[i] == '\v' || str[i] == '\f')
-				++i;
-			count = 0;
-			while (str[i] > 32 && str[i] < 127)
-			{
-				++count;
-				++i;
-			}
-			wrap[line][1] = malloc(count + 1);
-			if (!wrap[line][1])
-				return (NULL);
-			ft_strlcpy(wrap[line][1], &str[i - count], count + 1);
-			if (str[i] != '\n')
+			norm(&i, &count, str);
+			if (!norm4(wrap[line], count, i, str))
 				return (NULL);
 		}
 		++line;
@@ -101,7 +131,7 @@ int main()
 		printf("ERROR");
 		return 0;
 	}
-	char ***res = parse(str);
+	char ***res = parse(str, 0, 0, 0);
 	if(!res){
 		printf("ERROR");
 		return 0;
